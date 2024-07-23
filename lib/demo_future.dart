46202dcf800740a2fcb9adbd15fe007394352f42
phatdroid94 <phatdroid94@gmail.com>
2024-07-23 18:50:26 +0700

    Khoi tao future

--- lib/demo_future.dart
+++ lib/demo_future.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class DemoFuturePage extends StatefulWidget {
@@ -8,6 +10,28 @@ class DemoFuturePage extends StatefulWidget {
 }
 
 class _DemoFuturePageState extends State<DemoFuturePage> {
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    // Cho tat ca future co gia tri moi tra ve
+    // Future futureResult = Future.wait([
+    //   Future.delayed(Duration(seconds: 3), () => 1),
+    //   Future.delayed(Duration(seconds: 2), () => 2)
+    // ]);
+    //
+    // futureResult.then((listValue) => print(listValue));
+
+    // Tra ve ket qua cua future hoang thanh som nhat
+    // Future futureResult = Future.any([
+    //   Future.delayed(Duration(seconds: 3), () => 1),
+    //   Future.delayed(Duration(seconds: 2), () => 2)
+    // ]);
+    //
+    // futureResult.then((value) => print(value));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
