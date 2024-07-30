17a3a78520979bdaeeafd1c943c3701f3c5d8886
phatdroid94 <phatdroid94@gmail.com>
2024-07-30 18:37:40 +0700

    Handle error

--- lib/demo_future.dart
+++ lib/demo_future.dart
@@ -30,6 +30,37 @@ class _DemoFuturePageState extends State<DemoFuturePage> {
     // ]);
     //
     // futureResult.then((value) => print(value));
+
+   // 3 + 2 - 1 * 5
+   //  sum(3, 2).then((tong) {
+   //    minus(tong, 1).then((hieu) {
+   //      multiplication(hieu, 5).then((result) {
+   //        print(result);
+   //      });
+   //    });
+   //  });
+
+    sum(3, 1)
+        .then((tong) => minus(tong as int, 1))
+        .then((hieu) => multiplication(hieu, 5))
+        .then((result) {
+          throw Exception("abc");
+        })
+        .catchError((error) => print("pphat"));
+  }
+
+  Future<num> sum(int a, dynamic b) {
+    return Future.delayed(Duration(seconds: 1), () {
+      return a + (b as num);
+    });
+  }
+
+  Future<int> minus(int a, int b) {
+    return Future.delayed(Duration(seconds: 1), () => a - b);
+  }
+
+  Future<int> multiplication(int a, int b) {
+    return Future.delayed(Duration(seconds: 1), () => a * b);
   }
 
   @override
